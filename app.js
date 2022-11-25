1387cf3a466b86aa49729af7967dc5e7677939bd
ferny1005 <fernyindy@gmail.com>
2022-11-25 22:52:36 +0800

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 3000;
 app.use(express.json());
 app.use("/api", [postsRouter, commentsRouter]);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(port, 'Server is open with port!');
 });
 
